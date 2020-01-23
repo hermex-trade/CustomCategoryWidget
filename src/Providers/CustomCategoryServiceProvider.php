b40e1d21cd30d775da5c2517cca5fbc74b785f1c
Nicolas Almy <nicolas.almy@gmail.com>
2020-01-23 11:45:02 +0100

    First commit of widget

---
+++ src/Providers/CustomCategoryServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CustomCategoryWidget\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class CustomCategorySericeProvider extends ServiceProvider
+{
+  public function register()
+  {
+  }
+  public function boot()
+  {
+  }
+}
+
+?>
---
+++ src/Widgets/CustomCategoryWidget.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CustomCategoryWidget\Widgets;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class CustomCategoryWidget extends BaseWidget
+{
+    protected $template = "CustomCategoryWidget::Widgets.CustomCategoryWidget";
+}
+
+?>
